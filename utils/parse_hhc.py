f6060ed2762b9804f6bf5f0b7b073da68567c844
mac <mac@MacBook-Pro-mac.local>
2018-02-26 12:32:01 +0300

    Catch chapter headers in HHC parser.

--- utils/parse_hhc.py
+++ utils/parse_hhc.py
@@ -11,12 +11,21 @@ def parse_hhc(src, Trace=False):
             doc = BeautifulSoup(f, "html5lib")
             l = [] 
             d = []
+            head = ''
+            key = ''
+            val = ''
             for o in doc.findAll('object'):
                 if o['type'] == 'text/sitemap':
                     for param in o.findAll('param'):
                         if param['name'] == 'Local': key = param['value']
-                        if param['name'] == 'Name':  val = param['value']
-                        if param['name'] in [ 'Name', 'ImageNumber' ]:
+                        if param['name'] == 'Name':
+                            if val != '':
+                                """Chapter header detected, save it."""
+                                head = val
+                            else:
+                                head = ''
+                            val = param['value']
+                        if param['name'] != 'Name':
                             """Only the first item served."""
                             continue
                         path = '.'.join(reversed([p.name for p in param.parentGenerator() if p]))
@@ -24,8 +33,12 @@ def parse_hhc(src, Trace=False):
                         if depth not in d: d.append(depth)
                         if Trace:
                             print "{:10}|{:60}|{:10}".format(param.name,param.attrs, path)
-                    l.append((key, depth, val))
+                    if key != '':
+                        l.append((key, depth, val, None if head == '' else head))
+                        head = ''
+                        key = ''
+                        val = ''
 
         # Remap depth
         d2 = sorted(d)
-        return dumps([(t[0].lower(), d2.index(t[1]), t[2]) for t in l ]) 
+        return dumps([(t[0].lower(), d2.index(t[1]), t[2], t[3]) for t in l ]) 
